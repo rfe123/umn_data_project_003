29ca2477308a24052c29853bba718fb7e2083852
rfe123 <59402267+rfe123@users.noreply.github.com>
2024-01-29 20:36:50 -0600

    init flask api

---
+++ flask/flask_api.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite://parks_db.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine, reflect=True)
+
+# Save reference to the table
+print(Base.classes.keys())
+Passenger = Base.classes.passenger
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/api/data/<column_name>", methods=['GET'])
+def get_data(column_name):
+    # create session (link from python to db)
+    session = Session(engine)
+
+    # query the desired column
+    results = list(session.query(getattr(Passenger, column_name)))
+
+    # close the session
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_values = list(np.ravel(results))
+    response = jsonify([str(a) for a in all_values])
+
+    # https://stackoverflow.com/questions/26980713/solve-cross-origin-resource-sharing-with-flask
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+# run the app!
+if __name__ == '__main__':
+    app.run()
