8b0d0a993673bd9e1cec5896187ec32783df323f
rfe123 <59402267+rfe123@users.noreply.github.com>
2024-01-29 21:16:36 -0600

    Updated Loading - changing data types

--- flask/flask_api.py
+++ flask/flask_api.py
@@ -10,7 +10,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///parks_db_1.sqlite")
+engine = create_engine("sqlite:///parks_db_2.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -32,7 +32,7 @@ app = Flask(__name__)
 #################################################
 
 @app.route("/api/data/<column_name>", methods=['GET'])
-def get_data(column_name):
+def get_data_by_column(column_name):
     # create session (link from python to db)
     session = Session(engine)
 
@@ -50,6 +50,44 @@ def get_data(column_name):
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+@app.route("/api/all_data", methods=['GET'])
+def get_data():
+    # create session (link from python to db)
+    session = Session(engine)
+
+    # query the desired column
+    results = list(session.query(Cities).all())
+
+    # close the session
+    session.close()
+
+    # Convert list of tuples into normal list
+    #all_values = list(np.ravel(results))
+    # response = []
+    # for city in results:
+    #     city_dict = {
+    #         'id': city.id,
+    #         'name': city.City_Name,
+    #         'state': city.State_code
+    #     }
+    #     response.append(city_dict)
+
+        # Serialize the users into a list of dictionaries
+    city_list = []
+    for city in results:
+        city_dict = {}
+        for column in Cities.__table__.columns:
+            city_dict[column.name] = getattr(city, column.name)
+        city_list.append(city_dict)
+
+    # Return the JSON list
+    return jsonify(city_list)
+
+    # https://stackoverflow.com/questions/26980713/solve-cross-origin-resource-sharing-with-flask
+    #response.headers.add('Access-Control-Allow-Origin', '*')
+
+    # Return the JSON dictionary
+    return response
 
 # run the app!
 if __name__ == '__main__':
