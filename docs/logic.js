a3c4775dc68429ec10ee0574633b4c8e07437c7f
NadyaSlb <nslabuk1@email.cpcc.edu>
2024-01-30 19:58:45 -0500

    markers1

--- docs/logic.js
+++ docs/logic.js
@@ -9,8 +9,47 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-let city_data = d3.json('http://127.0.0.1:5000/api/all_data');
+let cities = [];
+let cityData = [];
 
-city_data.then(function(data) {
-    console.log(data);
-});
+// Load data from the Flask API
+d3.json('http://127.0.0.1:5000/api/all_data')
+    .then(function (data) {
+        const all_data = data[0];
+
+        all_data.forEach((element) => {
+            // Assuming City_Name and State_code are properties in each element
+            const city = element.City_Name;
+            const state = element.State_code;
+
+            // Add city and state to the cities array
+            cities.push({ city: city, state: state });
+        });
+
+        console.log(cities);
+
+        // Return the promise to continue the chain
+        return d3.json('city.list.json');
+    })
+    .then(function (localCityData) {
+        // Handle the loaded data
+        console.log(localCityData);
+
+        cities.forEach((element) => {
+            // Assuming City_Name is the property in each element
+            for (let i = 0; i < localCityData.length; i++) {
+                if (element.city === localCityData[i].name & element.state === localCityData[i].state) {
+                    console.log(localCityData[i].name);
+                    // Add the matching city data to the cityData array
+                    cityData.push({name:localCityData[i].name, coord:localCityData[i].coord});
+                }
+            }
+        });
+
+        console.log(cityData);
+
+        cityData.forEach(city => {
+            const marker = L.marker([city.coord.lat, city.coord.lon]).addTo(myMap);
+            marker.bindPopup(city.name);
+        });
+    });
